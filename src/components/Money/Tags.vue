08d4f8f910ec9a5b7ef3ff7155f13a7f8e561256
CodeBlankCjj <cj13480394274@163.com>
2021-05-31 19:41:54 +0800

    Tags.vue 使用 Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -21,13 +21,16 @@ import {Component} from 'vue-property-decorator';
 @Component({
   computed: {
     tagList() {
-      // return this.$store.fetchTags();
-      return [];
+      return this.$store.state.tagList;
     }
   }
 })
 export default class Tags extends Vue {
   selectedTags: string[] = [];
+
+  created() {
+    this.$store.commit('fetchTags');
+  }
 
   toggle(tag: string) {
     const index = this.selectedTags.indexOf(tag);
@@ -45,6 +48,7 @@ export default class Tags extends Vue {
       return window.alert('标签名不能为空');
     }
     //store.createTag(name);
+    this.$store.commit('createTag', name);
   }
 }
 </script>
