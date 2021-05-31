73a2601a32a8ca224767d09f275b5f50c63cab70
CodeBlankCjj <cj13480394274@163.com>
2021-05-31 14:10:28 +0800

    封裝 record 的增

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -11,22 +11,19 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component, Watch} from 'vue-property-decorator';
+import {Component} from 'vue-property-decorator';
 
 import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import Tags from '@/components/Money/Tags.vue';
 import FormItem from '@/components/Money/FormItem.vue';
-import recordListModel from '@/models/recordListModel';
-
-const recordList = recordListModel.fetch();
 
 @Component({
   components: {FormItem, Tags, Types, NumberPad},
 })
 export default class Money extends Vue {
   tags = window.tagList;
-  recordList = recordList;
+  recordList = window.recordList;
   record: RecordItem = {
     tags: [],
     notes: '',
@@ -43,12 +40,7 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    recordListModel.create(this.record);
-  }
-
-  @Watch('recordList')
-  onRecordListChange() {
-    recordListModel.save();
+    window.createRecord(this.record);
   }
 }
 </script>
