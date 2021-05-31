189a40a944da99a5d4ff61482c17d4b31d03bdc4
CodeBlankCjj <cj13480394274@163.com>
2021-05-31 23:17:39 +0800

    更新CSS

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -24,18 +24,20 @@ export default class Statistics extends Vue {
 </script>
 
 <style scoped lang="scss">
-::v-deep .type-tabs-item {
-  background: white;
+::v-deep {
+  .type-tabs-item {
+    background: white;
 
-  &.selected {
-    background: #c4c4c4;
+    &.selected {
+      background: #c4c4c4;
 
-    &::after {
-      display: none;
+      &::after {
+        display: none;
+      }
     }
   }
-}
-::v-deep .interval-tabs-item{
-  height: 48px;
+  .interval-tabs-item{
+    height: 48px;
+  }
 }
 </style>
