d2d382a3ac771ee30d3d3bbf48d1c03761584f68
CodeBlankCjj <cj13480394274@163.com>
2021-05-31 23:07:27 +0800

    在Money组件使用 Tabs

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout class-prefix="layout">
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
-    <Types :value.sync="record.type"/>
+    <Tabs :data-source="recordTypeList" :value.sync="record.type"/>
     <div class="notes">
       <FormItem field-name="备注" placeholder="请输入备注" @update:value="onUpdateNotes"/>
     </div>
@@ -17,9 +17,11 @@ import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import Tags from '@/components/Money/Tags.vue';
 import FormItem from '@/components/Money/FormItem.vue';
+import recordTypeList from '@/constants/recordTypeList';
+import Tabs from '@/components/Tabs.vue';
 
 @Component({
-  components: {FormItem, Tags, Types, NumberPad}
+  components: {Tabs, FormItem, Tags, Types, NumberPad}
 })
 export default class Money extends Vue {
   record: RecordItem = {
@@ -32,6 +34,8 @@ export default class Money extends Vue {
   get recordList() {
     return this.$store.state.recordList;
   }
+
+  recordTypeList = recordTypeList;
 
   created() {
     this.$store.commit('fetchRecords');
