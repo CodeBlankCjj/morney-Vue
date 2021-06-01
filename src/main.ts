e3d5fa23455492a13d7eb5769cfe11e36b4596ef
CodeBlankCjj <cj13480394274@163.com>
2021-06-01 17:16:17 +0800

    修改路径，解决二维码显示不了的问题

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ window.onload = function () {
 if (document.documentElement.clientWidth > 500) {
   window.alert('请使用手机打开本页面，以保证浏览效果');
   const img = document.createElement('img');
-  img.src = '/logo.png';
+  img.src = 'img/logo.png';
   img.style.position = 'fixed';
   img.style.left = '50%';
   img.style.top = '50%';
