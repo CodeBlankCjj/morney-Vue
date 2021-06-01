5be3d5d17fe31dc4ca4683e2d9dcd7dba9555707
CodeBlankCjj <cj13480394274@163.com>
2021-06-01 16:33:53 +0800

    保证用户使用手机打开页面

--- src/main.ts
+++ src/main.ts
@@ -20,8 +20,20 @@ new Vue({
   render: h => h(App)
 }).$mount('#app');
 
-window.onload= function (){
-  setTimeout(function (){
-    window.scroll(0,9999)
-  },0)
+window.onload = function () {
+  setTimeout(function () {
+    window.scroll(0, 9999);
+  }, 0);
+};
+
+if (document.documentElement.clientWidth > 500) {
+  window.alert('请使用手机打开本页面，以保证浏览效果');
+  const img = document.createElement('img');
+  img.src = '/logo.png';
+  img.style.position = 'fixed';
+  img.style.left = '50%';
+  img.style.top = '50%';
+  img.style.transform = 'translate(-50%, -50%)';
+  img.style.boxShadow = '0 0 10px rgba(0,0,0,0.25)';
+  document.body.append(img);
 }
